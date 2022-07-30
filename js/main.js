a3bf3aa14140684473dcbca239a185c16954b597
thinh <daothinh1105@gmail.com>
2022-07-30 13:49:36 +0700

    chinh trang index

--- js/main.js
+++ js/main.js
@@ -106,4 +106,14 @@
     });
     
 })(jQuery);
-
+/*them */
+function zoom(e){
+    var zoomer = e.currentTarget;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    x = offsetX/zoomer.offsetWidth*100
+    y = offsetY/zoomer.offsetHeight*100
+    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+  };
+  /*end*/
+  
